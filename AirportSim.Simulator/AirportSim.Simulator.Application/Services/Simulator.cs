11c7396ae86b566431b6ba3191770b9a0dbeb465
Ethan Shoham <ethan380s@gmail.com>
2021-12-07 12:44:29 +0200

    starting with simulator service

---
+++ AirportSim.Simulator/AirportSim.Simulator.Application/Services/Simulator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AirportSim.Simulator.Application.Services
+{
+    public class Simulator
+    {
+        private Timer eventTimer;
+        public Simulator()
+        {
+            eventTimer = new(10000);
+            eventTimer.AutoReset = true;
+            eventTimer.Elapsed += EventTimer_Elapsed;
+        }
+
+        private void EventTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
